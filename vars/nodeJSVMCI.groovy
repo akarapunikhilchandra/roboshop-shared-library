e5d2aec5198a0865e287e4fa809ccb40a396ff24
nikhil <anc@gmail.com>
2023-10-31 09:52:57 +0530

    all

--- vars/nodeJSVMCI.groovy
+++ vars/nodeJSVMCI.groovy
@@ -52,7 +52,7 @@ def call(Map configMap){
                     nexusArtifactUploader(
                         nexusVersion: 'nexus3',
                         protocol: 'http',
-                        nexusUrl: '54.198.236.192:8081/',
+                        nexusUrl: '3.85.195.206:8081/',
                         groupId: 'com.roboshop',
                         version: "$packageVersion",
                         repository: "${component}",
